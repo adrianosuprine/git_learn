92038790b25d9a86fbd1311f7eb68f52a50ab8e0
Ian <iankosgey535@gmail.com>
2024-02-20 23:38:12 +0300

    Added actors page

---
+++ src/Components/Actors.jsx
@@ -0,0 +1,22 @@
+import { actors } from "../data";
+
+function Actors() {
+  return (
+  <div> 
+    <h1>Actors Page</h1>
+    <div>
+        {actors.map((actor , index) => (
+          <div key={index}>
+            <p>Name: {actor.name}</p>
+            {actor.movies.map((movies, index) => (
+             <li key={index}>{movies}</li>
+            ))}
+          </div>
+        ))}
+      </div>
+  </div>
+
+  )
+}
+
+export default Actors;
